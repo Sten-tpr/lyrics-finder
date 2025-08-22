419ed33cf67b9dd514fcd4e0af748f8e35a69843
sten-tpr <stenfold@gmail.com>
2025-08-22 11:50:32 +0200

    [NEW] Initialize project

---
+++ src/main/java/com/music/lyrics_finder/LyricsFinderApplication.java
@@ -0,0 +1,13 @@
+package com.music.lyrics_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyricsFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyricsFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/music/lyrics_finder/LyricsFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.lyrics_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricsFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
